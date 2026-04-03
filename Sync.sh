8f07df49f2d552db2e106ac8960a7cf61ca1f2cb
Henrique <henrique@unknown9cda3e7f66b0.lan>
2026-04-03 20:26:28 +0100

    Bugfix: Exchange_Boundaries in last_line KPM_MOTOR
    This reverts commit 59b5be8a669486f5c2913a6124f04f0ea24cd7ba.

--- Sync.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avhzp --exclude 'build/' ~/devel/fork/sync/zv/ "viking:~/scratch/Tmp/"
--- slurm_tools/Loop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-THS=4
-Seed=1
-
-sbatch --cpus-per-task="$THS" --export=ALL,seed="$Seed" Launch.sh
